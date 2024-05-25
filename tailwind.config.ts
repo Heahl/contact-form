f5558f46d9940e2f5787872f692a24769cd1381c
heahl <hendrik712.a@gmail.com>
2024-05-25 21:29:09 +0200

    add layout, states, validation, error handling, success

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,17 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      fontFamily: {
+      colors: {
+        'green-200': 'hsl(148, 38%, 91%)',
+        'green-600': 'hsl(169, 82%, 27%)',
+        'red': 'hsl(0, 66%, 54%)',
+        'grey-500': 'hsl(186, 15%, 59%)',
+        'grey-900': 'hsl(187, 24%, 22%)',
+      },
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        karla: ["Karla", ...fontFamily.sans],
       },
     },
-  },
   plugins: [],
 } satisfies Config;
+  
